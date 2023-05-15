24611c33beef2a59a25fe323b7fe6d75e6a16337
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-15 15:46:17 -0400

    filtres

--- src/app/pages/accueil/components/filtres/filtres.component.ts
+++ src/app/pages/accueil/components/filtres/filtres.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { TypeBouteille } from 'src/app/models/models';
+import { Format, Pays, TypeBouteille } from 'src/app/models/models';
 import { CatalogueService } from 'src/app/services/catalogue.service';
 
 @Component({
@@ -9,8 +9,14 @@ import { CatalogueService } from 'src/app/services/catalogue.service';
 })
 export class FiltresComponent implements OnInit, OnDestroy {
   @Output() showType = new EventEmitter<string>();
+  @Output() showFormat = new EventEmitter<string>();
+  @Output() showPays = new EventEmitter<string>();
   typesSubscription: Subscription | undefined;
+  formatsSubscription: Subscription | undefined;
+  paysSubscription: Subscription | undefined;
   types: Array<TypeBouteille> | undefined;
+  formats: Array<Format> | undefined;
+  pays: Array<Pays> | undefined;
   constructor(private catalogueService: CatalogueService) { }
 
 
@@ -20,15 +26,35 @@ export class FiltresComponent implements OnInit, OnDestroy {
     .subscribe((response) => {
       this.types = response;
     });
+    this.formatsSubscription = this.catalogueService.getFormats()
+    .subscribe((response) => {
+      this.formats = response;
+    });
+    this.paysSubscription = this.catalogueService.getPays()
+    .subscribe((response) => {
+      this.pays = response;
+    });
   }
 
   onVoirType(type: TypeBouteille):void {
     this.showType.emit(type.type);
   }
+  onVoirFormat(format: Format):void {
+    this.showFormat.emit(format.format);
+  }
+  onVoirPays(pays: Pays):void {
+    this.showPays.emit(pays.pays);
+  }
 
   ngOnDestroy(): void {
       if(this.typesSubscription) {
         this.typesSubscription.unsubscribe();
+      }
+      if(this.formatsSubscription) {
+        this.formatsSubscription.unsubscribe();
+      }
+      if(this.paysSubscription) {
+        this.paysSubscription.unsubscribe();
       }
   }
 }

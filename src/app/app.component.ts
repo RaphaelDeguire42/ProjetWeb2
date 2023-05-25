f05427fd76f583077ac7cafba712f6dad038427d
RaphaelDeguire42 <e2295637@cmaisonneuve.qc.ca>
2023-05-24 22:42:21 -0400

    page connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Panier } from './models/models';
 import { PanierService } from './services/panier.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-entete [panier]="panier"></app-entete>
-    <router-outlet></router-outlet>
+  <ng-container *ngIf="!isConnexionRoute()">
+  <app-entete [panier]="panier"></app-entete>
+</ng-container>
+<router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent implements OnInit {
   panier: Panier = { items: [] };
 
-  constructor(private panierService: PanierService) {
+  constructor(private panierService: PanierService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.panierService.panier.subscribe((_panier) => {
       this.panier = _panier;
     })
+  }
+
+  isConnexionRoute(): boolean {
+    return this.router.url === '/connexion';
   }
 }

85490f7c80f1334b47a35be1e611d1f8115c6f63
Deguire <e2295637@b1128-30.cmaisonneuve.qc.ca>
2023-05-26 11:37:41 -0400

    regler relation avec lapi

--- src/app/services/catalogue.service.ts
+++ src/app/services/catalogue.service.ts
@@ -42,7 +42,7 @@ export class CatalogueService {
   constructor(private httpClient: HttpClient) { }
 
 
-  getAllBouteilles(limit = '12', prix?:string, types?: number[], formats?: number[], pays?: number[]): Observable<Array<Bouteille>> {
+  getAllBouteilles(limit = '12', prix?:string, types?: number[], formats?: number[], pays?: number[]): Observable<any> {
     let premier = true,
       queryString = '',
       queries = [
@@ -97,7 +97,7 @@ export class CatalogueService {
       premier = false;
     }
 
-    return this.httpClient.get<Array<Bouteille>>(`${CATALOGUE_BASE_URL}/bouteilles?
+    return this.httpClient.get<any>(`${CATALOGUE_BASE_URL}/bouteilles?
     ${queryString}`);
 
   }
